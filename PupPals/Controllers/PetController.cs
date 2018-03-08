87f3ba6e11eaec01dd5972cabcfbba31a3c2f58b
Kristen Norris <krnorris65@gmail.com>
2018-03-08 16:23:43 -0600

    user can add a pet and select the house from a dropdown list

---
+++ PupPals/Controllers/PetController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PupPals.Data;
+using PupPals.Models;
+using PupPals.Models.PetViewModels;
+
+namespace PupPals.Controllers
+{
+    public class PetController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PetController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // This task retrieves the currently authenticated user
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+
+        // GET: Pet
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pet.Include(p => p.House);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pet/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pet
+                .Include(p => p.House)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pet/Create
+        public async Task<IActionResult> Create()
+        {
+            //gets the current user
+            ApplicationUser _user = await GetCurrentUserAsync();
+
+            //displays list of houses
+            PetCreateViewModel houseList = new PetCreateViewModel(_context, _user);
+            //ViewData["HouseId"] = new SelectList(_context.House, "Id", "Address");
+            return View(houseList);
+        }
+
+        // POST: Pet/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Pet pet)
+        {
+            ModelState.Remove("User");
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await GetCurrentUserAsync();
+                pet.User = user;
+                _context.Add(pet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["HouseId"] = new SelectList(_context.House, "Id", "Address", pet.HouseId);
+    
+            //displays list of houses
+            //PetCreateViewModel houseList = new PetCreateViewModel(_context, _user);
+            return View(pet);
+        }
+
+        // GET: Pet/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pet.SingleOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            ViewData["HouseId"] = new SelectList(_context.House, "Id", "Address", pet.HouseId);
+            return View(pet);
+        }
+
+        // POST: Pet/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Type,Description,HouseId,MyPet,BestFriend,Notes,Photo")] Pet pet)
+        {
+            if (id != pet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PetExists(pet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HouseId"] = new SelectList(_context.House, "Id", "Address", pet.HouseId);
+            return View(pet);
+        }
+
+        // GET: Pet/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pet
+                .Include(p => p.House)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // POST: Pet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pet = await _context.Pet.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Pet.Remove(pet);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PetExists(int id)
+        {
+            return _context.Pet.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PupPals/Models/PetViewModels/PetCreateViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PupPals.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PupPals.Models.PetViewModels
+{
+    public class PetCreateViewModel
+    {
+        public string Name { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+        public List<SelectListItem> HouseList { get; set; }
+        public House House { get; set; }
+
+        public PetCreateViewModel(ApplicationDbContext ctx, ApplicationUser usr)
+        {
+
+            this.HouseList = ctx.House
+                                    .Where(h => h.User == usr)
+                                    .AsEnumerable()
+                                    .Select(li => new SelectListItem
+                                    {
+                                        Text = li.Address + ", " + li.City + ", " + li.State,
+                                        Value = li.Id.ToString()
+                                    }).ToList();
+
+            this.HouseList.Insert(0, new SelectListItem
+            {
+                Text = "select address",
+                Value = "0"
+            });
+        }
+
+
+        [Required]
+        public bool MyPet { get; set; }
+
+        public bool BestFriend { get; set; }
+
+        public string Notes { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
